d5c68cb1759e8cd8be246e318ca4e2fbf48498e0
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-02 15:57:18 -0700

    changed the way employees were made, created new lists

--- Lambda/Emp.cs
+++ Lambda/Emp.cs
@@ -4,16 +4,11 @@ using System.Text;
 
 namespace Lambda
 {
-    public class Emp
+    public class Employee
     {
         public string firstname { get; set; }
         public string lastname { get; set; }
         public int ID { get; set; }
-        public Emp(string _firstName, string _lastName, int _ID)
-        {
-            firstname = _firstName;
-            lastname = _lastName;
-            ID = _ID;
-        }
+        
     }
 }
--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -9,29 +9,77 @@ namespace Lambda
         static void Main(string[] args)
         {
 
-            List<Emp> employees = new List<Emp>();
-            List<string> firstnames = new List<string>()
-            {
-                "Joe", "Tim", "Lin", "Hon", "Joe", "Tristan", "Tayd", "Cole", "Kip", "Lebron", "Luka"
-            };
-            List<string> lastnames = new List<string>()
-            {
-                "Smo", "Tat", "Lan", "Solo", "Mo", "Hill", "Tot", "Smith", "More", "James", "Doncic"
-            };
-            List<int> IDs = new List<int>()
-            {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            };
-            int i = 0;
-            foreach(var firstName in firstnames)
-            {
-                employees.Add(new Emp(firstName, lastnames[i], IDs[i]));
-            }
-            foreach(Emp employee in employees)
-            {
-                if(employee.firstname == employee.firstname)
-            }
+            List<Employee> employees = new List<Employee>();
+            Employee employee1 = new Employee();
+            employee1.firstname = "Joe";
+            employee1.lastname = "Smo";
+            employee1.ID = 1;
+            employees.Add(employee1);
 
+            Employee employee2 = new Employee();
+            employee2.firstname = "Tim";
+            employee2.lastname = "Tat";
+            employee2.ID = 2;
+            employees.Add(employee2);
+
+            Employee employee3 = new Employee();
+            employee3.firstname ="Lin";
+            employee3.lastname = "Lan";
+            employee3.ID = 3;
+            employees.Add(employee3);
+
+            Employee employee4 = new Employee();
+            employee4.firstname = "Hon";
+            employee4.lastname = "Solo";
+            employee4.ID = 4;
+            employees.Add(employee4);
+
+            Employee employee5 = new Employee();
+            employee5.firstname = "Joe";
+            employee5.lastname = "Mo";
+            employee5.ID = 5;
+            employees.Add(employee5);
+
+            Employee employee6 = new Employee();
+            employee6.firstname = "Tristan";
+            employee6.lastname = "Hill";
+            employee6.ID = 6;
+            employees.Add(employee6);
+
+            Employee employee7 = new Employee();
+            employee7.firstname = "Tayd";
+            employee7.lastname = "Tot";
+            employee7.ID = 7;
+            employees.Add(employee7);
+
+            Employee employee8 = new Employee();
+            employee8.firstname = "Cole";
+            employee8.lastname = "Smith";
+            employee8.ID = 8;
+            employees.Add(employee8);
+
+            Employee employee9 = new Employee();
+            employee9.firstname = "Kip";
+            employee9.lastname = "More";
+            employee9.ID = 9;
+            employees.Add(employee9);
+
+            Employee employee10 = new Employee();
+            employee10.firstname = "Lebron";
+            employee10.lastname = "James";
+            employee10.ID = 10;
+            employees.Add(employee10);
+
+            foreach(Employee employee in employees)
+            {
+                if(employee.firstname == "Joe")
+                {
+                    List<Employee> newemployees = new List<Employee>();
+                    Console.WriteLine(newemployees);
+                }
+            }
+            List<Employee> joeEmployees = joeEmployees.Where(x => x > 5).ToList();
+            List<Employee> employID = employID.Where(ID > 5).ToList();
         }
     }
 }
