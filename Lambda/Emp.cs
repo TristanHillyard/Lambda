bae47c8b9fee8858d8ba5cda82729a0142f72d39
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-02 14:36:47 -0700

    Add project files.

---
+++ Lambda/Emp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda
+{
+    public class Emp
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int ID { get; set; }
+        public Emp(string _firstName, string _lastName, int _ID)
+        {
+            firstname = _firstName;
+            lastname = _lastName;
+            ID = _ID;
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Emp> employees = new List<Emp>();
+            List<string> firstnames = new List<string>()
+            {
+                "Joe", "Tim", "Lin", "Hon", "Joe", "Tristan", "Tayd", "Cole", "Kip", "Lebron", "Luka"
+            };
+            List<string> lastnames = new List<string>()
+            {
+                "Smo", "Tat", "Lan", "Solo", "Mo", "Hill", "Tot", "Smith", "More", "James", "Doncic"
+            };
+            List<int> IDs = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            int i = 0;
+            foreach(var firstName in firstnames)
+            {
+                employees.Add(new Emp(firstName, lastnames[i], IDs[i]));
+            }
+            foreach(Emp employee in employees)
+            {
+                if(employee.firstname == employee.firstname)
+            }
+
+        }
+    }
+}
